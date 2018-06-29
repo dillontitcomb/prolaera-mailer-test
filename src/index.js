8fe8c7297f2d981717c5616903bd83a7a12e1bae
Dillon <dillontitcomb@gmail.com>
2018-06-29 11:21:22 -0700

    Add basic import statement

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { engine } from '@prolaera/prolaeraemailtemplates/src/index.js';
+
+console.log("Hello!");
+console.log(engine);
+
+function doThing(something) {
+  engine(something);
+}
+
+
